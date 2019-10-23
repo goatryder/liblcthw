56f22429f611cf27b00fea5edc37500c064a9059
Ruslan <nova9ok@gmail.com>
2019-10-23 18:29:34 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch make_check
     Changes to be committed:
    	modified:   Makefile
    	modified:   src/lcthw/darray.c
    	modified:   src/lcthw/darray.h
    	new file:   src/lcthw/darray_algos.c
    	new file:   src/lcthw/darray_algos.h
    	modified:   src/lcthw/dbg.h
    	modified:   src/lcthw/list.c
    	modified:   src/lcthw/list.h
    	modified:   src/lcthw/list_algos.c
    	modified:   src/lcthw/list_algos.h
    	new file:   tests/darray_algos_tests.c
    	modified:   tests/darray_tests.c
    	modified:   tests/list_algos_tests.c
    	modified:   tests/list_tests.c
    	modified:   tests/minunit.h
    	modified:   tests/runtests.sh

--- src/lcthw/darray.h
+++ src/lcthw/darray.h
@@ -5,41 +5,28 @@
 #include <lcthw/dbg.h>
 
 typedef struct DArray {
-	int end;
-	int max;
-	size_t element_size;
-	size_t expand_rate;
-	void **contents; // pointer to pointer contents which is point to memory
-					// with an unspecified type
+    int end;
+    int max;
+    size_t element_size;
+    size_t expand_rate;
+    void **contents;
 } DArray;
-
-// contents is a pointer to whole array
-// *contents is a pointer to the first element it's equal to
-// contents[0]
-// contents[index] <=> *(content + index)
-
-// int* p;  variable p is pointer to integer type
-// int i;   integer value
-// int i2 = *p;  integer i2 is assigned with integer value that pointer p is pointing to
-// int* p2 = &i; pointer p2 will point to the address of integer i
-
-// int* p <=> int p*
 
 DArray *DArray_create(size_t element_size, size_t initial_max);
 
-void DArray_destroy(DArray *array);
+void DArray_destroy(DArray * array);
 
-void DArray_clear(DArray *array);
+void DArray_clear(DArray * array);
 
-int DArray_expand(DArray *array);
+int DArray_expand(DArray * array);
 
-int DArray_contract(DArray *array);
+int DArray_contract(DArray * array);
 
-int DArray_push(DArray *array, void *el);
+int DArray_push(DArray * array, void *el);
 
-void *DArray_pop(DArray *array); // WHY POINTER?
+void *DArray_pop(DArray * array);
 
-void DArray_clear_destroy(DArray *array);
+void DArray_clear_destroy(DArray * array);
 
 #define DArray_last(A) ((A)->contents[(A)->end - 1])
 #define DArray_first(A) ((A)->contents[0])
@@ -49,40 +36,42 @@ void DArray_clear_destroy(DArray *array);
 
 #define DEFAULT_EXPAND_RATE 300
 
-
-static inline void DArray_set(DArray *array, int i, void *el) //Why pointer
+static inline void DArray_set(DArray * array, int i, void *el)
 {
-	check(i < array->max, "darray attempt to set past max");
-	array->contents[i] = el;
+    check(i < array->max, "darray attempt to set past max");
+    if (i > array->end)
+        array->end = i;
+    array->contents[i] = el;
 error:
-	return;
+    return;
 }
 
-static inline void *DArray_get(DArray *array, int i) // here is pointer cause we return pointer of value from pointed darray
+static inline void *DArray_get(DArray * array, int i)
 {
-	check(i < array->max, "darray attemt to get past max");
-	return array->contents[i];
+    check(i < array->max, "darray attempt to get past max");
+    return array->contents[i];
 error:
-	return NULL;
+    return NULL;
 }
 
-static inline void *DArray_remove(DArray *array, int i)
+static inline void *DArray_remove(DArray * array, int i)
 {
-	void *el = array->contents[i];
-	
-	array->contents[i] = NULL;
-	
-	return el;
+    void *el = array->contents[i];
+
+    array->contents[i] = NULL;
+
+    return el;
 }
 
-static inline void *DArray_new(DArray *array) //we return pointer to new adress of darray which is size of argument darray
+static inline void *DArray_new(DArray * array)
 {
-	check(array->element_size > 0, "Can't use DArray_new on 0 size darrays.");
-	
-	return calloc(1, array->element_size);
-	
+    check(array->element_size > 0,
+            "Can't use DArray_new on 0 size darrays.");
+
+    return calloc(1, array->element_size);
+
 error:
-	return NULL;
+    return NULL;
 }
 
 #define DArray_free(E) free((E))
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,14 +1,15 @@
 echo "Running unit tests:"
 
-for i in tests/*_tests/
+for i in tests/*_tests
 do
-	if test -f $i
-	then
-		echo "ERROR in test $i: here's tests/tests.log"
-		echo "------"
-		tail tests/tests.log
-		exit 1
-	fi
+    $i 2>&1 >> tests/tests.log
+    if [ ! $? ]
+    then
+        echo "ERROR in test $i: here's tests/tests.log"
+        echo "------"
+        tail tests/tests.log
+        exit 1
+    fi
 done
 
 echo ""
