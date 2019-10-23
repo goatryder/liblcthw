b652caf3a08cd4afc5a051a88babe344b0529fb9
Ruslan <nova9ok@gmail.com>
2019-10-23 14:39:16 +0300

    modified:   tests/runtests.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -4,15 +4,10 @@ for i in tests/*_tests/
 do
 	if test -f $i
 	then
-		if $VALGRIND ./$i 2>> tests/tests.log
-		then
-			echo $i PASS
-		else
-			echo "ERROR in test $i: here's tests/tests.log"
-			echo "------"
-			tail tests/tests.log
-			exit 1
-		fi
+		echo "ERROR in test $i: here's tests/tests.log"
+		echo "------"
+		tail tests/tests.log
+		exit 1
 	fi
 done
 
